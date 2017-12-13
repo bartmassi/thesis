b0e9041e7446bc836a798c85b6b70e00c8b45dbd
bartmassi <bartmassi.jr@gmail.com>
2017-12-12 22:58:00 -0500

    sqlite3
    changed over psycopg2 code to sqlite3, for use with a sqlite database.

--- test.py
+++ test.py
@@ -9,17 +9,16 @@ creation date: 12-11-17
 #%load_ext autoreload
 #%autoreload 2
 
-import psycopg2 as pg2
+import sqlite3
 import pandas as pd
 import Plotter
 import analyzer
 import matplotlib.pyplot as plt
 import numpy as np
 
-conn = pg2.connect(database='arithmeticstudy', user='postgres',
-                   password=input('pw: '))
+dbfloc = 'D:\\Bart\\Dropbox\\science\\leelab\\projects\\Arithmetic\\data\\_curatedData\\'
+conn = sqlite3.connect(database=dbfloc+'arithmeticstudy.db')
 cur = conn.cursor()
-
 
 ###########Generate plot of accuracy in enforced addition and non-enforced addition trials.
 #EA trials are those where sum > singleton, but augend < singleton and addend < singleton.
@@ -44,26 +43,19 @@ query1 = '''
 #execute query, and convert output into a pandas table
 cur.execute(query1)
 dataout1 = cur.fetchall()
-colnames1 = [desc.name for desc in cur.description]
+colnames1 = [desc[0] for desc in cur.description]
 data1 = pd.DataFrame.from_records(dataout1,columns=colnames1)
 
 #Generate a scatterplot of the results. 
-xdict = {'data':data['nonea_pcorrect'],
-         'label':'Accuracy in non-EA trials',
-         'limits':(.5,1)}
-ydict = {'data':data['ea_pcorrect'],
-         'label':'Accuracy in EA trials',
-         'limits':(.5,1)}
-
 h,axes = plt.subplots(1,1)
-Plotter.scatter(axes,xdata=data1['nonea_pcorrect'],xlabel='Accuracy in non-EA trials',xlim=(.5,1),
-                ydata=data1['ea_pcorrect'],ylabel='Accuracy in EA trials',ylim=(.5,1),title='Animal Accuracy')
+Plotter.scatter(axes,xdata=data1['nonEA_pcorrect'],xlabel='Accuracy in non-EA trials',xlim=(.5,1),
+                ydata=data1['EA_pcorrect'],ylabel='Accuracy in EA trials',ylim=(.5,1),title='Animal Accuracy')
 
 
 ###########Fit a regression model to animals' choice data, and then plot coefficients.
 #SQL query
 query2 = '''
-        SELECT session,animal,CAST(chose_sum AS int),augend,addend,singleton
+        SELECT session,animal,CAST(chose_sum AS int) as chose_sum,augend,addend,singleton
         FROM behavioralstudy
         WHERE experiment = 'Addition'
         ORDER BY animal,session
@@ -71,7 +63,7 @@ query2 = '''
 #Execute query, then convert to pandas table
 cur.execute(query2)
 dataout2 = cur.fetchall()
-colnames2 = [desc.name for desc in cur.description]
+colnames2 = [desc[0] for desc in cur.description]
 data2 = pd.DataFrame.from_records(dataout2,columns=colnames2)
 
 #Fit regression model to separate sessions
